05b3413cc96c7855fcc43283baf098b3653e9d4c
Rajib Sarker <redhat0593@gmail.com>
2023-02-08 17:09:05 +0600

    all completing

---
+++ assets/js/custom.js
@@ -0,0 +1,13 @@
+const button = document.getElementById("menuButton");
+const closebutton = document.getElementById("closeButton");
+const content = document.getElementById("sideNav");
+
+button.addEventListener("click", () => {
+    content.style.width = "250px"
+    content.style.padding = "30px"
+})
+
+closebutton.addEventListener("click", () => {
+    content.style.width = "0px"
+    content.style.padding = "0px"
+})
